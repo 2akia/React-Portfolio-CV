752630fb5d3ace308a846537c0eba963885c424b
2akia <2akssultana@gmail.com>
2024-01-30 23:16:04 +0000

    added the contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,68 @@
-import React, {useState} from "react";
-
-
-
+import React, { useState } from "react";
+import "./Contact.css";
 
 export const Contact = () => {
-  return (
-    <div>  <h1>Contact</h1> </div>
-  
-  
-  )
 
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+  };
+
+  return (
+    <div>
+      <h1>Contact</h1>
+      <form onSubmit={handleSubmit} className="contact-form">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleInputChange}
+        ></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <div class="contactLinks">
+        <a class="text-decoration-none link-offset-2 link-offset-3-hover link-opacity-25-hover" href="https://accounts.google.com/">Email</a>
+        <a class="text-decoration-none link-offset-2 link-offset-3-hover link-opacity-25-hover" href="https://uk.linkedin.com/in/jakia-sultana-8353a1171">LinkedIn</a>
+        <a class="text-decoration-none link-offset-2 link-offset-3-hover link-opacity-25-hover" href="https://github.com/2akia?tab=repositories">GitHub</a>
+      </div>
+    </div>
+  );
 };
+
 
 export default Contact;
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -9,7 +9,7 @@ const Project = () => {
       <div className="row">
         <div className="col-md-6">
           <div className="card">
-            <img className="card-img-top" src=".\assets\screenshots\Work-Day-Scheduler.png" alt="Card image cap"/>
+            <img className="card-img-top" src="./assets/screenshots/Work-Day-Scheduler.png" alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">Work-Day-Scheduler</h5>
               <p className="card-text">We were instructed to create a simple calendar application that allows a user to save events for each hour of the day.</p>
@@ -20,7 +20,7 @@ const Project = () => {
 
         <div className="col-md-6">
           <div className="card">
-            <img className="card-img-top" src=".\assets\screenshots\Projects 3 screenshot.png" alt="Card image cap"/>
+            <img className="card-img-top" src="./assets/screenshots/Projects 3 screenshot.png" alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">Bootstrap-Portfolio</h5>
               <p className="card-text">We were instructed to create a website and this will be a portfolio website using Bootstrap.</p>
@@ -31,7 +31,7 @@ const Project = () => {
 
         <div className="col-md-6">
           <div className="card">
-            <img className="card-img-top" src=".\assets\screenshots\Password-Generator.png" alt="Card image cap"/>
+            <img className="card-img-top" src="./assets/screenshots/Password-Generator.png" alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">Password-Generator</h5>
               <p className="card-text">This challenge required us to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code.</p>
@@ -43,7 +43,7 @@ const Project = () => {
        
         <div className="col-md-6">
           <div className="card">
-            <img className="card-img-top" src=".\assets\screenshots\Team profile generator oop.png" alt="Card image cap"/>
+            <img className="card-img-top" src="./assets/screenshots/Team profile generator oop.png" alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">Team-Profile-Generator-OOP</h5>
               <p className="card-text">The application is a node.js command-line which is intended to gather information about members of a software engineering team through user input and that would generate an HTML webpage that presents summaries for each team member.</p>
@@ -56,7 +56,7 @@ const Project = () => {
 
         <div className="col-md-6">
           <div className="card">
-            <img className="card-img-top" src=".\assets\screenshots\Capital-City-Quiz.png" alt="Card image cap"/>
+            <img className="card-img-top" src="./assets/screenshots/Capital-City-Quiz.png" alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">Capital-City-Quiz</h5>
               <p className="card-text">We were tasked with creating a timed quiz on JavaScript fundamentals that stores high scores.</p>
@@ -67,7 +67,7 @@ const Project = () => {
 
         <div className="col-md-6">
           <div className="card">
-            <img className="card-img-top" src=".\assets\screenshots\project 4 Screenshot.png" alt="Card image cap"/>
+            <img className="card-img-top" src="./assets/screenshots/project 4 Screenshot.png" alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">Console-Finances</h5>
               <p className="card-text">This project is about when we were tasked with creating code for analyzing the financial records of a company.</p>
